dc599ec0e3acee2d01c66da9d06e9cbd8016ad74
vikneshoftheleaf <vikneshoftheleaf@gmail.com>
2024-05-23 11:43:48 +0530

    initial layout2

--- src/app/(landing page)/page.js
+++ src/app/(landing page)/page.js
@@ -8,12 +8,7 @@ export const metadata = {
 export default function Home() {
   return (
     <main>
-      <div className="w-full flex justify-end gap-4 p-4">
-      <Link href={'login'}>Login</Link>
-      <Link href={'signup'}>Signup</Link>
-      <Link href={'dashboard'}>Dashboard</Link>
-
-      </div>
+      
     </main>
 
   );
--- src/app/components/nav.js
+++ src/app/components/nav.js
@@ -4,15 +4,23 @@ import { useRouter } from "next/navigation";
 import Link from "next/link";
 export default function Navbar() {
     const router = useRouter();
-    if(router.pathname != "dashboard")
-    return (
+    if (router.pathname != "dashboard")
+        return (
 
-            <nav className="w-full flex justify-end gap-4 p-4">
-                <Link href={'login'}>Login</Link>
-                <Link href={'signup'}>Signup</Link>
-                <Link href={'dashboard'}>Dashboard</Link>
+            <nav className="w-full flex justify-between gap-4 py-4 px-8 items-center">
+                <div>
+                    <Link href={'/'}>Logo</Link>
+                </div>
+                <div>
 
+                </div>
+                <div className="flex justify-end gap-4 items-center">
+                    <Link href={'dashboard'}>Dashboard</Link>
+                    <Link href={'login'}>Login</Link>
+                    <Link className="bg-emerald-600 text-slate-50 px-4 py-2 rounded-full" href={'signup'}>Signup</Link>
+
+                </div>
             </nav>
-       
-    )
+
+        )
 }
