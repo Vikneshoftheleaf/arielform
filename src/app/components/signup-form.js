4df028128dfa3c4dd6d8d55eba795d3c0d60c81e
vikneshoftheleaf <vikneshoftheleaf@gmail.com>
2024-05-30 05:48:39 +0530

    firebase

--- src/app/components/signup-form.js
+++ src/app/components/signup-form.js
@@ -1,5 +1,4 @@
 "use client"
-<<<<<<< HEAD
 import Link from "next/link";
 import { useAuthContext } from "../../context/auth-context"
 import { useRouter } from "next/navigation";
@@ -34,83 +33,81 @@ const SignupForm = () => {
     function emailSignup(email, password) {
         //e.preventDefault();
         createUserWithEmailAndPassword(auth, email, password)
-          .then((userCredential) => {
-            // Signed up 
-            const user = userCredential.user;
-            const displayName = user.email.split('@')
-            setDoc(doc(db, "user", user.uid), {
-              uid: user.uid,
-              userName:null,
-              displayName: user.displayName ? user.displayName : displayName[0],
-              email: user.email,
-              photoURL: user.photoURL ? user.photoURL : null,
-              verified: false,
+            .then((userCredential) => {
+                // Signed up 
+                const user = userCredential.user;
+                const displayName = user.email.split('@')
+                setDoc(doc(db, "user", user.uid), {
+                    uid: user.uid,
+                    userName: null,
+                    displayName: user.displayName ? user.displayName : displayName[0],
+                    email: user.email,
+                    photoURL: user.photoURL ? user.photoURL : null,
+                    verified: false,
+                })
+                console.log('siggned in with email!')
+                // ...
             })
-            console.log('siggned in with email!')
-            // ...
-          })
-          .catch((error) => {
-            const errorCode = error.code;
-            const errorMessage = error.message;
-            console.log(errorMessage)
-            const errMsg = errorCode.split('/')      
-            // ..
-          });
+            .catch((error) => {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                console.log(errorMessage)
+                const errMsg = errorCode.split('/')
+                // ..
+            });
 
-      }
+    }
 
 
-      function googleSignup() {
+    function googleSignup() {
         signInWithPopup(auth, provider)
-          .then((result) => {
-            // This gives you a Google Access Token. You can use it to access the Google API.
-            const credential = GoogleAuthProvider.credentialFromResult(result);
-            const token = credential.accessToken;
-            // The signed-in user info.
-            const user = result.user;
-            const [displayName] = user.email.split('@')
-            setDoc(doc(db, "user", user.uid), {
-              uid: user.uid,
-              userName:null,
-              displayName: user.displayName ? user.displayName : displayName[0],
-              email: user.email,
-              photoURL: user.photoURL ? user.photoURL : null,
-              verified: false,
-      
-            }).then(()=>sendCode('welcome',null, user.email, user.displayName));
-            // IdP data available using getAdditionalUserInfo(result)
-            console.log("signned up with google!")
-      
-            // ...
-          }).catch((error) => {
-            // Handle Errors here.
-            const errorCode = error.code;
-            const errorMessage = error.message;
-            // The email of the user's account used.
-            const email = error.customData.email;
-            // The AuthCredential type that was used.
-            const credential = GoogleAuthProvider.credentialFromError(error);
-            
-            console.log(errorMessage)
-            // ...
-          });
-      }
+            .then((result) => {
+                // This gives you a Google Access Token. You can use it to access the Google API.
+                const credential = GoogleAuthProvider.credentialFromResult(result);
+                const token = credential.accessToken;
+                // The signed-in user info.
+                const user = result.user;
+                const [displayName] = user.email.split('@')
+                setDoc(doc(db, "user", user.uid), {
+                    uid: user.uid,
+                    userName: null,
+                    displayName: user.displayName ? user.displayName : displayName[0],
+                    email: user.email,
+                    photoURL: user.photoURL ? user.photoURL : null,
+                    verified: false,
+
+                }).then(() => sendCode('welcome', null, user.email, user.displayName));
+                // IdP data available using getAdditionalUserInfo(result)
+                console.log("signned up with google!")
+
+                // ...
+            }).catch((error) => {
+                // Handle Errors here.
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                // The email of the user's account used.
+                const email = error.customData.email;
+                // The AuthCredential type that was used.
+                const credential = GoogleAuthProvider.credentialFromError(error);
+
+                console.log(errorMessage)
+                // ...
+            });
+    }
 
     function handleSubmit() {
 
-        if(!terms)
-        {
+        if (!terms) {
 
             setErrorMsg("Accept the terms to continue")
         }
-        else if(password != confirmPassword)
-        {
+        else if (password != confirmPassword) {
             setErrorMsg("Check your passwords.")
         }
-        else{
-            emailSignup(email,password);
+        else {
+            emailSignup(email, password);
         }
-        
+
 
 
     }
@@ -155,7 +152,7 @@ const SignupForm = () => {
                                         </div>
                                     </div>
                                     <button onClick={() => handleSubmit()} class="w-full text-white bg-emerald-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-emerald-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Create an account</button>
-                                    <button onClick={()=> googleSignup()} class="w-full text-emerald-950 bg-emerald-50 border hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-emerald-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Continue with Google</button>
+                                    <button onClick={() => googleSignup()} class="w-full text-emerald-950 bg-emerald-50 border hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-emerald-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Continue with Google</button>
 
                                     <p class="text-sm font-light text-gray-500 dark:text-gray-400">
                                         Already have an account? <Link href={'login'} class="font-medium text-primary-600 hover:underline dark:text-primary-500">Login here</Link>
@@ -168,55 +165,54 @@ const SignupForm = () => {
 
             </div>
         );
-=======
-const SignupForm = () => {
-    return (
-        <div>
-              <section class="bg-gray-50 dark:bg-gray-900">
-  <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-      <a href="#" class="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
-          <img class="w-8 h-8 mr-2" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg" alt="logo"/>
-          Flowbite    
-      </a>
-      <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
-          <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
-              <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-                  Create an account
-              </h1>
-              <div class="space-y-4 md:space-y-6">
-                  <div>
-                      <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-                      <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required=""/>
-                  </div>
-                  <div>
-                      <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-                      <input type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required=""/>
-                  </div>
-                  <div>
-                      <label for="confirm-password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Confirm password</label>
-                      <input type="confirm-password" name="confirm-password" id="confirm-password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required=""/>
-                  </div>
-                  <div class="flex items-start">
-                      <div class="flex items-center h-5">
-                        <input id="terms" aria-describedby="terms" type="checkbox" class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-emerald-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-emerald-600 dark:ring-offset-gray-800" required=""/>
-                      </div>
-                      <div class="ml-3 text-sm">
-                        <label for="terms" class="font-light text-gray-500 dark:text-gray-300">I accept the <a class="font-medium text-primary-600 hover:underline dark:text-primary-500" href="#">Terms and Conditions</a></label>
-                      </div>
-                  </div>
-                  <button class="w-full text-white bg-emerald-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-emerald-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Create an account</button>
-                  <p class="text-sm font-light text-gray-500 dark:text-gray-400">
-                      Already have an account? <Link href={'login'} class="font-medium text-primary-600 hover:underline dark:text-primary-500">Login here</Link>
-                  </p>
-              </div>
-          </div>
-      </div>
-  </div>
-</section>
-            
-        </div>
-    );
->>>>>>> 91fe5c8b4b9a2eaabf7c0992e43c8dda26e4b39b
-}
 
-export default SignupForm;
+    const SignupForm = () => {
+        return (
+            <div>
+                <section class="bg-gray-50 dark:bg-gray-900">
+                    <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+                        <a href="#" class="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
+                            <img class="w-8 h-8 mr-2" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg" alt="logo" />
+                            Flowbite
+                        </a>
+                        <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+                            <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
+                                <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                                    Create an account
+                                </h1>
+                                <div class="space-y-4 md:space-y-6">
+                                    <div>
+                                        <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
+                                        <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required="" />
+                                    </div>
+                                    <div>
+                                        <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+                                        <input type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="" />
+                                    </div>
+                                    <div>
+                                        <label for="confirm-password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Confirm password</label>
+                                        <input type="confirm-password" name="confirm-password" id="confirm-password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="" />
+                                    </div>
+                                    <div class="flex items-start">
+                                        <div class="flex items-center h-5">
+                                            <input id="terms" aria-describedby="terms" type="checkbox" class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-emerald-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-emerald-600 dark:ring-offset-gray-800" required="" />
+                                        </div>
+                                        <div class="ml-3 text-sm">
+                                            <label for="terms" class="font-light text-gray-500 dark:text-gray-300">I accept the <a class="font-medium text-primary-600 hover:underline dark:text-primary-500" href="#">Terms and Conditions</a></label>
+                                        </div>
+                                    </div>
+                                    <button class="w-full text-white bg-emerald-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-emerald-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Create an account</button>
+                                    <p class="text-sm font-light text-gray-500 dark:text-gray-400">
+                                        Already have an account? <Link href={'login'} class="font-medium text-primary-600 hover:underline dark:text-primary-500">Login here</Link>
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+
+            </div>
+        );
+    }
+}
+    export default SignupForm;
