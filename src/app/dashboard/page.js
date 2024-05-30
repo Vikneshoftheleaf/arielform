d6fe3d6156dc207f1888753596330fc78f89b892
vikneshoftheleaf <vikneshoftheleaf@gmail.com>
2024-05-30 14:52:29 +0530

    alpha1

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -1,11 +1,13 @@
+import DashboardScreen from "@/components/dashboard-screen"
+
+
 export const metadata = {
    title: "Dashboard | ArielForm",
    description: "explore all the features of ArielForm"
 }
-import DashboardScreen from "@/components/dashboard-screen"
 export default function Dashboard() {
    return (
-      <DashboardScreen/>
+      <DashboardScreen></DashboardScreen>
 
    )
 }
